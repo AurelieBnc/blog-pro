cc02234cd6e5d5e46802096d44fa03a7bc65880a
AurelieB <a@a.a>
2023-03-12 18:10:45 +0100

    Create file myEnvironmentTwig + function activeClass()

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -3,22 +3,32 @@ namespace App\Controllers;
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use App\Core\MyExtensionTwig;
 
 abstract Class Controller
 {
     private $loader;
     protected $twig;
+    private $page = 'home';
 
     public function __construct()
     {
-        // on paramètre le dossier contenant nos templates
+        //Set the folder containing templates
         $this->loader = new FilesystemLoader(__DIR__.'/../Templates');
 
-        // on paramètre l'environnement Twig
+        //Set Twig Environnement
         $this->twig = new Environment($this->loader, [
             'debug' => true,
             'cache' => false, //'../tmp',
         ]);
+
+        //Current page name setting
+        if (isset($_GET['p']))
+        {
+            $this->page = $_GET['p'];
+        }
+        $this->twig->addGlobal('current_page', $this->page);
+        $this->twig->addExtension(new MyExtensionTwig());
     }
 
 }
