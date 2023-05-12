33b1c64d7126be78669ef9cf020915da067843cb
AurelieB <a@a.a>
2023-05-12 23:55:43 +0200

    add disableUser method on adminController

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -49,4 +49,41 @@ use App\Entity\User;
     {
         $this->twig->display('admin/manageUser.twig', ['ROOT' => $this->root,'session' => $_SESSION]);
     }
+
+    public function disableUser()
+    {
+        $disableUserId = $_POST['userId'];
+        $model = new User;
+        $disableUser = new User;
+
+        if (isset($_POST['userId']) && !empty($_POST['userId']) && isset($_POST['is_verified'])) {
+            $disableUser = $model->setIs_verified($_POST['is_verified']);
+            $id = "id = ".$disableUserId;
+            $model->update($id, $disableUser);
+            echo "données bien mise à jour";
+
+        }
+
+        $user = new User;
+        $usersList = $user->findAll();
+
+        $model = new User;
+        $user = $model->find($_SESSION['id']);
+
+        $post = new Post;
+        $posts = $post->findAll();
+
+        $comment = new Comment;
+        $userDataComment = ['id_user' => $_SESSION['id']];
+        $comments = $comment->findBy($userDataComment);
+
+        $this->twig->display('admin/index.twig', [
+            'user' => $user,
+            'users' => $usersList,
+            'posts' => $posts,
+            'comments' => $comments,
+            'ROOT' => $this->root,
+            'session' => $_SESSION
+        ]);
+    }
 }
