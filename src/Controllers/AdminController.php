25095a82547dd3e8dc5d406f1319c9e89348cad7
AurelieB <a@a.a>
2023-05-13 00:24:42 +0200

    add deleteUser method in AdminController

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -86,4 +86,49 @@ use App\Entity\User;
             'session' => $_SESSION
         ]);
     }
+
+    public function deleteUser()
+    {
+        if ($_SESSION['logUser'] === 'admin' && isset($_POST['userId']) && !empty($_POST['userId'])) {
+
+            $deleteUserId = $_POST['userId'];
+            $deleteUser = new User;
+
+            //anonnymisation des commentaires
+            $data = ['id_user'=> $deleteUserId];
+            $modelComment = new Comment;
+            $listComments = $modelComment->findBy($data);
+
+            foreach ( $listComments as $comment ) {
+                $id = "id_user = ".$deleteUserId;
+                $comment = $modelComment->setId_user(2);
+                $comment = $modelComment->update($id, $comment );
+            }
+
+            $deleteUser->delete($deleteUserId);
+            echo "utilisateur supprimé";
+        }
+
+        $user = new User;
+        $usersList = $user->findAll();
+
+        $model = new User;
+        $user = $model->find($_SESSION['id']);
+
+        $post = new Post;
+        $posts = $post->findAll();
+
+        $comment = new Comment;
+        $userDataComment = ['id_user' => $_SESSION['id']];
+        $comments = $comment->findBy($userDataComment);
+
+        $this->twig->display('admin/index.twig', [
+            'user' => $user,
+            'users' => $usersList,
+            'posts' => $posts,
+            'comments' => $comments,
+            'ROOT' => $this->root,
+            'session' => $_SESSION
+        ]);
+    }
 }
